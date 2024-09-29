6f5860e4e5af4bc0aa1730f134bbb96c45f9496f
João Vitor Sousa Ramos <79048652+Kaneliodas@users.noreply.github.com>
2024-09-29 15:03:39 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,5 @@
+#4.6 – Números ímpares: Use o terceiro argumento da função range() para criar
+#uma lista de números ímpares de 1 a 20. Utilize um laço for para exibir todos
+#os números.
+lista = list(range(1,21,2))
+for a in lista: print(a)
